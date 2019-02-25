763e756673aec94941b7f62dd1397e12bde67ea0
Luis Barroso Vázquez <lbarrosov@gmail.com>
2019-02-26 00:40:26 +0100

    test 1

--- lib/common/services/ServicesInfo.dart
+++ lib/common/services/ServicesInfo.dart
@@ -2,7 +2,7 @@ class ServicesInfo{
 
   //service common params
   static const PARAM_API_KEY = 'apikey';
-  static const PARAM_API_KEY_VALUE = 'USE_YOUR_OWN_API_KEY_AND_HASH';
+  static const PARAM_API_KEY_VALUE = '093a569a831710608553aa64018ad108';
   static const PARAM_HASH = 'hash';
   static const PARAM_HASH_VALUE = 'dfddc471e4398fe423e4ecbf2f4bb137';
   static const PARAM_TS = 'ts';
@@ -11,10 +11,14 @@ class ServicesInfo{
   //service characters
   static const SERVICE_CHARACTERS = "v1/public/characters";
   static const CHARACTERS_PARAM_LIMIT = 'limit';
-  static const CHARACTERS_PARAM_LIMIT_VALUE = '80';
+  static const CHARACTERS_PARAM_LIMIT_VALUE = '20';
 
   //service comics
   static const SERVICE_COMICS = 'v1/public/comics';
   static const COMICS_PARAM_LIMIT = 'limit';
-  static const COMICS_PARAM_LIMIT_VALUE = '80';
+  static const COMICS_PARAM_LIMIT_VALUE = '20';
+
+  //Azure
+  static const CLIENT_ID = "527795e1-1a1f-492f-bfd2-e17b18c981bc";
+  static const CLIENT_SECRET = "JaSKXuzwSXarO13O2q8ZV+jD/KRorBW2oOkjRxWYct4=";
 }
--- lib/mainScreen/MainScreen.dart
+++ lib/mainScreen/MainScreen.dart
@@ -1,9 +1,13 @@
+import 'package:aad_oauth/model/config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test_marvel_api/common/icons/CustomIcons.dart';
 import 'package:flutter_test_marvel_api/common/resources/CustomColors.dart';
 import 'package:flutter_test_marvel_api/common/resources/StringsConstants.dart';
+import 'package:flutter_test_marvel_api/common/services/ServicesInfo.dart';
 import 'package:flutter_test_marvel_api/mainScreen/tabsCharacters/ui/TabCharacters.dart';
 import 'package:flutter_test_marvel_api/mainScreen/tabsComics/ui/TabComics.dart';
+
+import 'package:aad_oauth/aad_oauth.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -13,8 +17,8 @@ class MainScreen extends StatefulWidget {
 }
 
 class MainScreenState extends State<MainScreen> {
-
   int bottomNavigationBarIndex = 0;
+  bool firstTime = true;
 
   final List<Widget> listScreen = [
     TabCharacters(),
@@ -24,8 +28,42 @@ class MainScreenState extends State<MainScreen> {
     Text("Stories"),
   ];
 
+  Future<AadOAuth> loginOwn(AadOAuth oauth) async{
+    await oauth.login();
+    return oauth;
+  }
+
+  Future<String> getTokenOwn(AadOAuth oauth) async{
+    var resultToken = await oauth.getAccessToken();
+    return resultToken;
+  }
+
+  getToken(){
+    var tenantId = "47ab2d80-6ec1-4abd-9a97-8bf01b634f1d";
+    var clientId = "527795e1-1a1f-492f-bfd2-e17b18c981bc";
+
+    final Config config = new Config(tenantId, clientId, "http://flutterRules");
+    final AadOAuth oauth = new AadOAuth(config);
+    Future<AadOAuth> a = loginOwn(oauth);
+    Future<String> accessToken = getTokenOwn(oauth);
+    print("TOKEN: ${accessToken}");
+  }
+
   @override
   Widget build(BuildContext context) {
+    //https://login.microsoftonline.com/common/adminconsent?client_id=6731de76-14a6-49ae-97bc-6eba6914391e&state=12345&redirect_uri=http://localhost/myapp/permissions
+
+    //https://login.microsoftonline.com/47ab2d80-6ec1-4abd-9a97-8bf01b634f1d/oauth2/token
+
+    //final Config config = new Config("YOUR TENANT ID", "YOUR CLIENT ID", "openid profile offline_access");
+    //final Config config = new Config(ServicesInfo.CLIENT_SECRET, ServicesInfo.CLIENT_SECRET, "http://localhost:");
+
+    if(firstTime){
+      firstTime = false;
+      getToken();
+    }
+    
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: CustomColors.COLOR_RED,
@@ -37,8 +75,9 @@ class MainScreenState extends State<MainScreen> {
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         fixedColor: CustomColors.COLOR_RED,
-        currentIndex: bottomNavigationBarIndex, // this will be set when a new tab is tapped
-        onTap: (int index){
+        currentIndex:
+            bottomNavigationBarIndex, // this will be set when a new tab is tapped
+        onTap: (int index) {
           setState(() {
             bottomNavigationBarIndex = index;
           });
