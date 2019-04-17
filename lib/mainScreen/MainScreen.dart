3ff17612fde8449c66ff2331c34da93876d6cca0
Luis Barroso Vazquez <luis.barroso.vazquez@accenture.com>
2019-04-17 18:07:25 +0200

    Replaced bottom bar.

--- lib/mainScreen/MainScreen.dart
+++ lib/mainScreen/MainScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test_marvel_api/common/icons/CustomIcons.dart';
 import 'package:flutter_test_marvel_api/common/resources/CustomColors.dart';
 import 'package:flutter_test_marvel_api/common/resources/StringsConstants.dart';
+import 'package:flutter_test_marvel_api/common/widget/ColouredBottomBar/ColouredBarIcon.dart';
+import 'package:flutter_test_marvel_api/common/widget/ColouredBottomBar/ColouredBottomBarWidget.dart';
 import 'package:flutter_test_marvel_api/mainScreen/tabsCharacters/ui/TabCharacters.dart';
 import 'package:flutter_test_marvel_api/mainScreen/tabsComics/ui/TabComics.dart';
 
@@ -13,64 +15,53 @@ class MainScreen extends StatefulWidget {
 }
 
 class MainScreenState extends State<MainScreen> {
-
   int bottomNavigationBarIndex = 0;
 
   final List<Widget> listScreen = [
     TabCharacters(),
     TabComics(),
-    Text("Creators"),
-    Text("Events"),
-    Text("Stories"),
+    //TODO future tabs with content
+    // Text("Creators"),
+    // Text("Events"),
+    // Text("Stories"),
   ];
+
+  ColouredBottomBarWidget getColourBar() {
+    return ColouredBottomBarWidget(
+      indexButtonActive: bottomNavigationBarIndex,
+      durationAnimationColor: 1500,
+      durationAnimationSize: 1000,
+      activeIconColor: Colors.white,
+      disableIconColor: Colors.black,
+      onIndexChanged: (index) {
+        setState(() {
+          bottomNavigationBarIndex = index;
+        });
+      },
+      itemList: [
+        ColouredBarIcon(
+            title: StringsConstants.MAIN_SCREEN_CHARACTERS,
+            iconSrc: CustomIcons.ICON_CHARACTER,
+            color: Colors.red),
+        ColouredBarIcon(
+            title: StringsConstants.MAIN_SCREEN_COMICS,
+            iconSrc: CustomIcons.ICON_COMIC,
+            color: Colors.blue),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: CustomColors.COLOR_RED,
+        backgroundColor: CustomColors.COLOR_GREY,
         title: Text(StringsConstants.MAIN_SCREEN_TITLE),
       ),
       body: Center(
         child: listScreen[bottomNavigationBarIndex],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        fixedColor: CustomColors.COLOR_RED,
-        currentIndex: bottomNavigationBarIndex, // this will be set when a new tab is tapped
-        onTap: (int index){
-          setState(() {
-            bottomNavigationBarIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset(CustomIcons.ICON_CHARACTER,
-                width: 40.0, height: 40.0),
-            title: new Text(StringsConstants.MAIN_SCREEN_CHARACTERS),
-          ),
-          BottomNavigationBarItem(
-            icon:
-                Image.asset(CustomIcons.ICON_COMIC, width: 40.0, height: 40.0),
-            title: new Text(StringsConstants.MAIN_SCREEN_COMICS),
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(CustomIcons.ICON_CREATOR,
-                width: 40.0, height: 40.0),
-            title: new Text(StringsConstants.MAIN_SCREEN_CREATORS),
-          ),
-          BottomNavigationBarItem(
-            icon:
-                Image.asset(CustomIcons.ICON_EVENT, width: 40.0, height: 40.0),
-            title: new Text(StringsConstants.MAIN_SCREEN_EVENTS),
-          ),
-          BottomNavigationBarItem(
-            icon:
-                Image.asset(CustomIcons.ICON_STORY, width: 40.0, height: 40.0),
-            title: new Text(StringsConstants.MAIN_SCREEN_STORIES),
-          ),
-        ],
-      ),
+      bottomNavigationBar: getColourBar(),
     );
   }
 }
